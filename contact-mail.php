6517b3cf6382483d9c9993ae8b84bc160c750fbd
Hyrull <xavier.leonard@outlook.com>
2023-10-19 11:22:56 +0200

    V1.11 (fixed form)

--- contact-mail.php
+++ contact-mail.php
@@ -1,14 +1,15 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $to = 'contact@etsdupont.fr'; // Replace with the actual email address of the website owner
-    $subject = 'Contact site -' . $_POST['Objet'];
+    $to = 'contact@etsdupont.fr';
+    $subject = 'Contact site -' . $_POST['nom'] . " - " . $_POST['Objet'] ;
     $message = "Prénom: " . $_POST['prenom'] . "\n"
         . "Nom de famille: " . $_POST['nom'] . "\n"
         . "E-mail: " . $_POST['mail'] . "\n"
         . "Numéro de téléphone: " . $_POST['telephone'] . "\n"
-        . "Objet de la demande: ". $_POST['nom'] - . $_POST['Objet']"\n"
+        . "Objet de la demande: " . $_POST['Objet'] . "\n"
         . "Message: " . $_POST['message'];
-    $headers = 'From: formulaire@etsdupont.fr' . "\r\n" .
+    $headers = 'From: Formulaire-site@etsdupont.fr' . "\r\n" .
+        "Content-Type: text/plain; charset=UTF-8\r\n" .
         'Reply-To: ' . $_POST['mail'] . "\r\n" .
         'X-Mailer: PHP/' . phpversion();
 
